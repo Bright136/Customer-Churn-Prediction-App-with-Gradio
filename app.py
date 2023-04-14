224d6fe4f6c5c360d1e27f16d48d8d7255ac16ed
Bright Eshun <eshunbright136@gmail.com>
2023-04-14 17:30:34 +0000

    Added more to App interface

--- app.py
+++ app.py
@@ -1,76 +1,87 @@
 import gradio as gr
 import pickle 
 from gradio.themes.base import Base
-import time
+# import time
+import pandas as pd
+import numpy as np
 
-# # Define your Gradio block with added rows
 
 
-# gender:['Female', 'Male']
-# SeniorCitizen:[0, 1]
-# Partner:['Yes', 'No']
-# Dependents:['No', 'Yes']
-# PhoneService:['No', 'Yes']
-# MultipleLines:['No phone service', 'No', 'Yes']
-# InternetService:['DSL', 'Fiber optic', 'No']
-# OnlineSecurity:['No', 'Yes', 'No internet service']
-# OnlineBackup:['Yes', 'No', 'No internet service']
-# DeviceProtection:['No', 'Yes', 'No internet service']
-# TechSupport:['No', 'Yes', 'No internet service']
-# StreamingTV:['No', 'Yes', 'No internet service']
-# StreamingMovies:['No', 'Yes', 'No internet service']
-# Contract:['Month-to-month', 'One year', 'Two year']
-# PaperlessBilling:['Yes', 'No']
-# PaymentMethod:['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)']
-# Churn:['No', 'Yes']
-# with gr.Blocks(theme=gr.themes.Default(primary_hue="red", secondary_hue="pink")
+
+
+
 
 def load_pickle(filename):
     with open(filename, 'rb') as file:
         data = pickle.load(file)
         return data
-    
+
 pipeline = load_pickle('full_pipeline.pkl')
 model = load_pickle('logistic_reg_class_model.pkl')
 
-class Seafoam(Base):
-    pass
+train_features = ['gender', 'SeniorCitizen', 'Partner', 'Dependents','tenure', 'PhoneService', 'MultipleLines', 'InternetService', 
+                   'OnlineSecurity', 'OnlineBackup', 'DeviceProtection','TechSupport','StreamingTV', 'StreamingMovies', 
+                   'Contract', 'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges', 'TotalCharges']
 
-seafoam = Seafoam()
 
-with gr.Blocks() as demo:
+
+theme = gr.themes.Base()
+with  gr.Blocks(theme=theme) as demo:
     gr.Markdown(
     """
     # Welcome Cherished User 👋 !
+    ## Customer Churn Classification App
     Start predicting customer churn.
     """)
     with gr.Row():
-        gender = gr.Dropdown(label='Gender', choices=['Female', 'Male'], )
-        contract  = gr.Dropdown(label='Contract', choices=['Month-to-month', 'One year', 'Two year'])
-        internet = gr.Dropdown(label='Gender', choices=['DSL', 'Fiber optic', 'No'])
+        gender = gr.Dropdown(label='Gender', choices=['Female', 'Male'])
+        Contract  = gr.Dropdown(label='Contract', choices=['Month-to-month', 'One year', 'Two year'])
+        InternetService = gr.Dropdown(label='Internet Service', choices=['DSL', 'Fiber optic', 'No'])
+
     with gr.Accordion('Yes or no'):
 
         with gr.Row():
             OnlineSecurity = gr.Radio(label="Online Security", choices=["Yes", "No", "No internet service"])
-            OnlineBackup = gr.Radio(label="Online Backup", choices=["Yes", "No"])
-            DeviceProtection = gr.Radio(label="Device Protection", choices=["Yes", "No"])
-            TechSupport = gr.Radio(label="Tech Support", choices=["Yes", "No"])
-            StreamingTV = gr.Radio(label="TV Streaming", choices=["Yes", "No"])
-            StreamingMovies = gr.Radio(label="Movie Streaming", choices=["Yes", "No"]) 
+            OnlineBackup = gr.Radio(label="Online Backup", choices=["Yes", "No", "No internet service"])
+            DeviceProtection = gr.Radio(label="Device Protection", choices=["Yes", "No", "No internet service"])
+            TechSupport = gr.Radio(label="Tech Support", choices=["Yes", "No", "No internet service"])
+            StreamingTV = gr.Radio(label="TV Streaming", choices=["Yes", "No", "No internet service"])
+            StreamingMovies = gr.Radio(label="Movie Streaming", choices=["Yes", "No", "No internet service"]) 
         with gr.Row():
             SeniorCitizen = gr.Radio(label="Senior Citizen", choices=["Yes", "No"])
             Partner = gr.Radio(label="Partner", choices=["Yes", "No"])
             Dependents = gr.Radio(label="Dependents", choices=["Yes", "No"])
             PaperlessBilling = gr.Radio(label="Paperless Billing", choices=["Yes", "No"])
             PhoneService = gr.Radio(label="Phone Service", choices=["Yes", "No"])
-            MultipleLines = gr.Radio(label="Multiple Lines", choices=["Yes", "No"]) 
+            MultipleLines = gr.Radio(label="Multiple Lines", choices=["No phone service", "Yes", "No"]) 
+    
     with gr.Row():
         MonthlyCharges = gr.Number(label="Monthly Charges")
         TotalCharges = gr.Number(label="Total Charges")
+        Tenure = gr.Number(label='Months of Tenure')
         PaymentMethod = gr.Dropdown(label="Payment Method", choices=["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"])
+
     submit_button = gr.Button('Prediction')
+    print(type([[122, 456]]))
+    
+    with gr.Row():
+        with gr.Accordion('Churn Prediction'):
+            output1 = gr.Slider(maximum=1,
+                                minimum=0,
+                                value=0.0,
+                                label='Yes')
+            output2 = gr.Slider(maximum=1,
+                                minimum=0,
+                                value=0.0,
+                                label='No')
 
 
 
+    # if submit_button:
+    #     print(predict_churn(gender, SeniorCitizen, Partner, Dependents, Tenure, PhoneService, MultipleLines, InternetService, 
+    #               OnlineSecurity, OnlineBackup, DeviceProtection,TechSupport,StreamingTV, StreamingMovies, 
+    #               Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges))
 
+# demo = gr.Interface(fn=predict_churn, inputs=[gender, SeniorCitizen, Partner, Dependents, Tenure, PhoneService, MultipleLines,
+#                                                    InternetService, OnlineSecurity, OnlineBackup, DeviceProtection,TechSupport,StreamingTV, StreamingMovies, Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges], outputs=['slider', 'slider'], theme=theme)
 
