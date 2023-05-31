2a08ab8bb1ed8a1d6a9936e92b484bb587a1b5b6
Bright Eshun <eshunbright136@gmail.com>
2023-05-31 15:08:52 +0000

    Added comments to the codes

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
+# Add the parent directory of the current file to the system path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
 
 import gradio as gr
 import pickle 
@@ -10,92 +12,104 @@ import pandas as pd
 import numpy as np
 from src.utils import create_new_columns, create_processed_dataframe
 
+# Get the directory path of the current file
 DIRPATH = os.path.dirname(os.path.realpath(__file__))
 
+# Define the file paths for the pickle files and the CSV file
 pipeline_pkl = os.path.join(DIRPATH, "..", "assets", "ml_components", "full_pipeline.pkl")
 log_reg = os.path.join(DIRPATH, "..","assets", "ml_components", "logistic_reg_class_model.pkl")
-
 hist_df = os.path.join(DIRPATH, "..", "assets",  "history.csv")
 
+# Function to check if a CSV file exists and append data to it
 def check_csv(csv_file, data):
     if os.path.isfile(csv_file):
+        # If the file exists, append the data to it without writing the header
         data.to_csv(csv_file, mode='a', header=False, index=False, encoding='utf-8')
     else:
+        # If the file doesn't exist, create a new file and write the data to it
         history = data.copy()
         history.to_csv(csv_file, index=False)
 
-
-
-
-
-
-
-def tenure_values():
-    cols = ['0-2', '3-5', '6-8', '9-11', '12-14', '15-17', '18-20', '21-23', '24-26', '27-29', '30-32', '33-35', '36-38', '39-41', '42-44', '45-47', '48-50', '51-53', '54-56', '57-59', '60-62', '63-65', '66-68', '69-71', '72-74']
-    return cols
-
-def predict_churn(gender, SeniorCitizen, Partner, Dependents, Tenure, PhoneService, MultipleLines, InternetService, 
-                  OnlineSecurity, OnlineBackup, DeviceProtection,TechSupport,StreamingTV, StreamingMovies, 
-                  Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges):
-    
-    data = [gender, SeniorCitizen, Partner, Dependents, Tenure, PhoneService, MultipleLines, InternetService, 
-                   OnlineSecurity, OnlineBackup, DeviceProtection,TechSupport,StreamingTV, StreamingMovies, 
-                   Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges]
-    
-    x = np.array([data])
-    dataframe = pd.DataFrame(x, columns=train_features)
-    dataframe = dataframe.astype({'MonthlyCharges': 'float', 'TotalCharges': 'float', 'tenure': 'float'})
-    dataframe_ = create_new_columns(dataframe)
-    try:
-        processed_data = pipeline.transform(dataframe_)
-    except Exception as e:
-        raise gr.gradio('Kindly make sure to check/select all')
-    else:
-        check_csv(hist_df, dataframe)
-        history = pd.read_csv(hist_df)
-
-        processed_dataframe = create_processed_dataframe(processed_data, dataframe)
-        predictions = model.predict_proba(processed_dataframe)
-    return round(predictions[0][0], 3), round(predictions[0][1], 3), history
-
-
-
-theme = gr.themes.Default().set(body_background_fill="#0E1117",
-                                 background_fill_secondary="#FFFFFF",
-                                 background_fill_primary="#262730",
-                                 body_text_color="#000000",
-                                 checkbox_background_color='#FFFFFF', 
-                                 slider_color_dark="#0000FF")
-
-
+# Function to load data from a pickle file
 def load_pickle(filename):
     with open(filename, 'rb') as file:
         data = pickle.load(file)
         return data
 
+# Load the pipeline and model from the pickle files
 pipeline = load_pickle(pipeline_pkl)
 model = load_pickle(log_reg)
 
+
+def predict_churn(gender, SeniorCitizen, Partner, Dependents, Tenure, PhoneService, MultipleLines, InternetService, 
+                  OnlineSecurity, OnlineBackup, DeviceProtection,TechSupport,StreamingTV, StreamingMovies, 
+                  Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges):
+    # Create a list of input data
+    data = [gender, SeniorCitizen, Partner, Dependents, Tenure, PhoneService, MultipleLines, InternetService, 
+                   OnlineSecurity, OnlineBackup, DeviceProtection,TechSupport,StreamingTV, StreamingMovies, 
+                   Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges]
+    
+    # Convert the list to a NumPy array
+    x = np.array([data])
+    
+    # Create a DataFrame from the array with column names
+    dataframe = pd.DataFrame(x, columns=train_features)
+    
+    # Convert specific columns to float data type
+    dataframe = dataframe.astype({'MonthlyCharges': 'float', 'TotalCharges': 'float', 'tenure': 'float'})
+    
+    # Create new columns in the DataFrame
+    dataframe_ = create_new_columns(dataframe)
+    
+    try:
+        # Apply the pipeline transformation to the processed DataFrame
+        processed_data = pipeline.transform(dataframe_)
+    except Exception as e:
+        raise gr.gradio('Kindly make sure to check/select all')
+    else:
+        # Check if the history.csv file exists and append the input data to it
+        check_csv(hist_df, dataframe)
+        
+        # Read the history.csv file into a DataFrame
+        history = pd.read_csv(hist_df)
+        
+        # Create a processed DataFrame from the processed_data
+        processed_dataframe = create_processed_dataframe(processed_data, dataframe, pipeline)
+        
+        # Make predictions using the model
+        predictions = model.predict_proba(processed_dataframe)
+    
+    # Return the churn predictions, history DataFrame, and input history
+    return round(predictions[0][0], 3), round(predictions[0][1], 3), history
+
+
+# Set the theme for the Gradio interface
+theme = gr.themes.Default().set(body_background_fill="#0E1117",
+                                 background_fill_secondary="#FFFFFF",
+                                 background_fill_primary="#262730",
+                                 body_text_color="#FF4B4B",
+                                 checkbox_background_color='#FFFFFF', 
+                                 button_secondary_background_fill="#FF4B4B")
+
+# Define the list of train features
 train_features = ['gender', 'SeniorCitizen', 'Partner', 'Dependents','tenure', 'PhoneService', 'MultipleLines', 'InternetService', 
                    'OnlineSecurity', 'OnlineBackup', 'DeviceProtection','TechSupport','StreamingTV', 'StreamingMovies', 
                    'Contract', 'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges', 'TotalCharges']
 
-
-# theme = gr.themes.Base()
+# Create the Gradio interface
 with  gr.Blocks(theme=theme) as demo:
     gr.HTML("""
     <h1 style="color:white; text-align:center">Customer Churn Classification App</h1>
     <h2 style="color:white;">Welcome Cherished User 👋 </h2>
     <h4 style="color:white;">Start predicting customer churn.</h4>
-    
     """)
+    
     with gr.Row():
         gender = gr.Dropdown(label='Gender', choices=['Female', 'Male'])
         Contract  = gr.Dropdown(label='Contract', choices=['Month-to-month', 'One year', 'Two year'])
         InternetService = gr.Dropdown(label='Internet Service', choices=['DSL', 'Fiber optic', 'No'])
 
     with gr.Accordion('Yes or no'):
-
         with gr.Row():
             OnlineSecurity = gr.Radio(label="Online Security", choices=["Yes", "No", "No internet service"])
             OnlineBackup = gr.Radio(label="Online Backup", choices=["Yes", "No", "No internet service"])
@@ -118,7 +132,6 @@ with  gr.Blocks(theme=theme) as demo:
         PaymentMethod = gr.Dropdown(label="Payment Method", choices=["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"])
 
     submit_button = gr.Button('Prediction')
-    print(type([[122, 456]]))
     
     with gr.Row():
         with gr.Accordion('Churn Prediction'):
@@ -137,6 +150,5 @@ with  gr.Blocks(theme=theme) as demo:
                                                   InternetService, OnlineSecurity, OnlineBackup, DeviceProtection,TechSupport,StreamingTV, StreamingMovies, Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges], outputs=[output1, output2, output3])
 
 
-# demo = gr.Interface(fn=predict_churn, inputs=[gender, SeniorCitizen, Partner, Dependents, Tenure, PhoneService, MultipleLines,
-#                                                    InternetService, OnlineSecurity, OnlineBackup, DeviceProtection,TechSupport,StreamingTV, StreamingMovies, Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges], outputs=['slider', 'slider'], theme=theme)
+
 
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,37 @@
 import pandas as pd
 import numpy as np
 
-
-
+# Function to create new columns in the train_data dataframe
 def create_new_columns(train_data):
+    # Calculate monthly variations by subtracting the product of tenure and monthly charges from total charges
     train_data['Monthly Variations'] = (train_data.loc[:, 'TotalCharges']) -((train_data.loc[:, 'tenure'] * train_data.loc[:, 'MonthlyCharges']))
+    
+    # Create labels for tenure groups
     labels =['{0}-{1}'.format(i, i+2) for i in range(0, 73, 3)]
+    
+    # Categorize tenure into groups using pd.cut and assign the labels to the tenure_group column
     train_data['tenure_group'] = pd.cut(train_data['tenure'], bins=(range(0, 78, 3)), right=False, labels=labels)
+    
+    # Drop the original tenure column
     train_data.drop(columns=['tenure'], inplace=True)
+    
+    # Return the modified train_data dataframe
     return train_data
 
 
-
-
+# Function to create a processed dataframe from processed_data, train_data, and preprocessor
 def create_processed_dataframe(processed_data, train_data, preprocessor):
-    train_num_cols=train_data.select_dtypes(exclude=['object', 'category']).columns
+    # Get the numerical columns from the train_data dataframe
+    train_num_cols = train_data.select_dtypes(exclude=['object', 'category']).columns
+    
+    # Get the names of categorical features from the preprocessor
     cat_features = preprocessor.named_transformers_['categorical']['cat_encoder'].get_feature_names()
+    
+    # Concatenate the numerical and categorical feature names to create labels
     labels = np.concatenate([train_num_cols, cat_features])
+    
+    # Create a new dataframe from the processed_data array and assign the labels as column names
     processed_dataframe = pd.DataFrame(processed_data.toarray(), columns=labels)
+    
+    # Return the processed_dataframe
     return processed_dataframe
